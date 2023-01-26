f3b7739765c3b569de178de300dfa5e3eff6ee2a
Kelsei Kyser <kkelsei127@yahoo.com>
2023-01-26 15:18:32 -0500

    updated the front end

--- src/roles.js
+++ src/roles.js
@@ -20,20 +20,20 @@ function mapEmployee(teamArray, role, createRole){
 //create cards dynamically for manager
 function createManager(manager){
     return `
-    <div class="col s4">
-        <div class="card cyan lighten-3">
-            <div class="card content teal lighten-5 center-align">
-                <h2 class="padding-top card-title">${manager.getName()}</h2>
+    <div class="col s4 z-depth-5">
+        <div class="card amber darken-2 z-depth-5">
+            <div class="card content orange darken-4 center-align">
+                <h2 class="padding-top card-title orange darken-4">${manager.getName()}</h2>
             </div>
             <div class="card-content">
                 <div class="card-content">
                     <span>ID: ${manager.getId()}</span>
                 </div>
                 <div class="card-content">
-                    <span>Email:<a class='black-text' href='mailto:${manager.getEmail()}?subject=We have a problem!'>${manager.getEmail()}</a></span>
+                    <span>Email:<a href='mailto:${manager.getEmail()}?subject=We have a problem!'>${manager.getEmail()}</a></span>
                 </div>
                 <div class="card-content">
-                    <span>GitHub:<a href='https://github.com/${manager.getOfficeNumber()}' target='_blank' rel='noreferrer'> ${manager.getOfficeNumber()}</a></span>
+                    <span>Office: ${manager.getOfficeNumber()}</span>
                 </div>
             </div>
         </div>
@@ -46,17 +46,17 @@ function createManager(manager){
 //create cards for engineer dynamically
 function createEngineer(engineer){
     return `
-    <div class="col s4">
-        <div class="card cyan lighten-3">
-            <div class="card content teal lighten-5 center-align">
-                <h2 class="padding-top card-title">${engineer.getName()}</h2>
+    <div class="col s4 z-depth-5">
+        <div class="card amber darken-2 z-depth-5">
+            <div class="card content orange darken-4 center-align">
+                <h2 class="padding-top card-title orange darken-4">${engineer.getName()}</h2>
             </div>
             <div class="card-content">
                 <div class="card-content">
                     <span>ID: ${engineer.getId()}</span>
                 </div>
                 <div class="card-content">
-                    <span>Email:<a class='black-text' href='mailto:${engineer.getEmail()}?subject=Help needed!'> ${engineer.getEmail()}</a></span>
+                    <span>Email:<a href='mailto:${engineer.getEmail()}?subject=Help needed!'> ${engineer.getEmail()}</a></span>
                 </div>
                 <div class="card-content">
                     <span>GitHub:<a href='https://github.com/${engineer.getGithub()}' target='_blank' rel='noreferrer'> ${engineer.getGithub()}</a></span>
@@ -72,19 +72,19 @@ function createEngineer(engineer){
 function createIntern(intern){
     return `
     <div class="col s4">
-        <div class="card cyan lighten-3">
-            <div class="card content teal lighten-5 center-align">
-                <h2 class="padding-top card-title">${intern.getName()}</h2>
+        <div class="card amber darken-2 z-depth-5">
+            <div class="card content orange darken-4 center-align">
+                <h2 class="padding-top card-title orange darken-4">${intern.getName()}</h2>
             </div>
             <div class="card-content">
                 <div class="card-content">
                     <span>ID: ${intern.getId()}</span>
                 </div>
                 <div class="card-content">
-                    <span>Email:<a class='black-text' href='mailto:${intern.getEmail()}?subject=You're doing great, sweetie!'> ${intern.getEmail()}</a></span>
+                    <span>Email:<a href='mailto:${intern.getEmail()}?subject=You're doing great, sweetie!'> ${intern.getEmail()}</a></span>
                 </div>
                 <div class="card-content">
-                    <span>GitHub:<a href='https://github.com/${intern.getSchool()}' target='_blank' rel='noreferrer'> ${intern.getSchool()}</a></span>
+                    <span>School: ${intern.getSchool()}</span>
                 </div>
             </div>
         </div>
@@ -124,16 +124,15 @@ function createHTML(employees) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Builder!</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" />        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="./assets/css/style.css">
     </head>
-    <body>
-        <header class='cyan lighten-1'>
-            <div class='cyan lighten-1'>
+    <body class='red darken-4'>
+        <header class='red darken-4'>
+            <div class='red darken-4'>
                 <h1 class='center-align'>Welcome to my team!</h1>
             </div>
         </header>
         <main>
-            <section class='cards-section'>
+            <section class='cards-section row'>
                 ${createHTMLTemplate(employees)}
             </section>
         </main>
